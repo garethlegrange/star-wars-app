673530475b73eadffd0909aa271345a7b3d5be1c
Gareth le Grange <garethlegrange@users.noreply.github.com>
2024-10-11 14:05:48 +0000

    redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,5 @@
-import { Typography } from "@mui/material";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <Container className="min-h-screen flex flex-col items-center justify-center" sx={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      gap: "2rem",
-      minHeight: "100vh",
-    }}>
-      <Box>
-        <Typography variant="h1">Gareth le Grange</Typography>
-        <Typography>Assessment for the lead front-end developer.</Typography>
-        <ul>
-          <li>
-            <Link href="/movies/">Task 1</Link>
-          </li>
-          <li>
-            <Link href="/gallery">Task 2</Link>
-          </li>
-        </ul>
-      </Box>
-    </Container>
-  );
+export default async function HomePage() {
+  redirect("/movies");
 }
